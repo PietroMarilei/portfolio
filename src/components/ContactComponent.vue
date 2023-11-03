3f78c87864da514f1571bd9d19997b74db3d4d79
PietroMarilei~ <pietromrl0@gmail.com>
2023-11-03 17:28:16 +0100

    //

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -7,6 +7,7 @@ export default {
     data() {
         return {
             store,
+            emailSent : false
         }
     },
     methods: {
@@ -30,7 +31,7 @@ export default {
         <!-- form-------------- -->
         <div class="formContainer">
             <form ref="form" @submit.prevent="sendEmail">
-                <div>
+                <div s>
                     <label for="">_your name:</label>
                     <input type="text" name="user_name" class="d-block" placeholder="type your name">
                 </div>
@@ -39,7 +40,8 @@ export default {
                     <label for="">_your e-mail:</label>
                     <input type="email" name="user_email" class="d-block" placeholder="type your email">
                 </div>
-                <div class="textAreaCont">
+                
+                <div class="d-block">
                     <label for="">_say Hi:</label>
                     <textarea name="message" placeholder="Type your message." class="textArea"></textarea>
                 </div>
@@ -93,7 +95,11 @@ form {
     input {
         all: unset;
         display: block;
+         width: 100%;
+        font-size: 0.8rem;
         padding: 0.3rem;
+        margin: 0.5rem 0;
+        padding-right: 0;
         border: 2px solid #366686;
         border-radius: 5px;
         background-color: #010c16;
@@ -101,10 +107,12 @@ form {
     textarea {
         border: 2px solid #366686;
         border-radius: 5px;
-        width: 100%;
+        // ??
+        width: 103%;
         background-color: #010c16;
     }
     input[type="submit"] {
+        width: unset;
         padding: 0.3rem 0.5rem;
         background-color: #192f44;
     }
