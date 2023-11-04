5607d0d0b447ca807de4928847dbea9374ac5662
PietroMarilei~ <pietromrl0@gmail.com>
2023-11-04 20:10:42 +0100

    add description

--- src/store.js
+++ src/store.js
@@ -5,48 +5,48 @@ export const store = reactive(
         skills: [
             {
                 name: 'HTML',
-                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'HTML, or HyperText Markup Language, serves as the backbone of web content. It provides the structural foundation for web pages, defining elements such as headings, paragraphs, links, and images. As a markup language, HTML enables the organization and presentation of information on the internet, acting as a fundamental building block for web development.',
                 icon: 'devicon-html5-plain',
             },
             {
                 name: '(S)CSS',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: '(SCSS), or Cascading Style Sheets, is a styling language that complements HTML by controlling the visual presentation of web pages. SCSS extends CSS with features like variables and nested rules, making stylesheets more maintainable and modular. It plays a crucial role in defining the layout, colors, and typography to enhance the user experience.',
                 icon: 'devicon-css3-plain',
             },
             {
                 name: 'Bootstrap',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'Bootstrap is a front-end framework that simplifies web development by providing a set of pre-designed components and styles. Developed by Twitter, Bootstrap streamlines the creation of responsive, mobile-friendly websites. It includes a grid system, navigation bars, modals, and other UI elements, allowing developers to build visually appealing and consistent interfaces.',
                 icon: 'devicon-bootstrap-plain',
             },
             {
                 name: 'Javascript',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'JavaScript is a versatile scripting language primarily used for client-side web development. It enables dynamic content, interactivity, and manipulation of the Document Object Model (DOM). JavaScript is a key component in creating modern, interactive web applications, often used in conjunction with HTML and CSS to deliver a seamless user experience.',
                 icon:  "devicon-javascript-plain" ,
             },
 
             {
                 name: 'Vue.js',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'Vue.js is a progressive JavaScript framework for building user interfaces. It focuses on the view layer of web applications, providing a flexible and efficient way to create interactive components. Vue.js is known for its simplicity and ease of integration, making it an excellent choice for both small projects and large-scale applications.',
                 icon: 'devicon-vuejs-plain',
             },
             {
                 name: 'PHP',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'PHP, Hypertext Preprocessor, is a server-side scripting language designed for web development. It facilitates the creation of dynamic web pages by embedding code within HTML. PHP is widely used for tasks such as form processing, database management, and server-side scripting, making it a cornerstone of back-end development.',
                 icon: 'devicon-php-plain',
             },
             {
                 name: 'MySQL',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'MySQL is an open-source relational database management system (RDBMS) that stores and manages structured data. It is a popular choice for web applications, providing scalability, reliability, and robust support for SQL queries. MySQL is often integrated with server-side languages like PHP to create dynamic, data-driven websites.',
                 icon: 'devicon-mysql-plain',
             },
             {
                 name: 'Laravel',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'Laravel is a PHP web application framework known for its elegant syntax and developer-friendly features. It follows the model-view-controller (MVC) pattern, streamlining the development process and promoting code organization. Laravel includes built-in tools for tasks such as routing, authentication, and database management, making it a powerful choice for building modern web applications.',
                 icon: 'devicon-laravel-plain',
             },
             {
                 name: 'Git',
-                description: 'CSSSSSSSSLorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dolor maxime distinctio reprehenderit alias repudiandae! Esse, vero doloremque! Maxime, necessitatibus? Minima, temporibus! Natus accusantium sunt velit porro, et voluptatum sint?',
+                description: 'Git is a distributed version control system that enables efficient and collaborative software development. It tracks changes in source code, allowing multiple developers to work on a project simultaneously without conflicts. Git\'s branching and merging capabilities provide a robust system for managing codebase versions, fostering collaboration and ensuring project integrity.',
                 icon: 'devicon-git-plain',
             },
 
