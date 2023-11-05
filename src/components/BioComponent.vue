71a55b5be99990bfc87923aa785d11771921a7bf
PietroMarilei~ <pietromrl0@gmail.com>
2023-11-05 19:09:19 +0100

    working on bio

--- src/components/BioComponent.vue
+++ src/components/BioComponent.vue
@@ -9,91 +9,43 @@ export default {
         }
     },
     methods: {
-        trick(){
-            console.log('lol');
-            // alert('hai vinto')
-        }
+       
     },
     mounted() {
-        var div = document.getElementById('movableDiv');
-        var distance = 55;
-       document.addEventListener('mousemove', function (event) {            
-            distance += event.movementX > 0 ? 10 : -10;
-
-            // Imposta i limiti per la variabile distance
-            distance = Math.max(0, distance); // per evitare valori negativi
-            distance = Math.min(100, distance); // o qualsiasi limite desiderato
-
-            // Aggiorna la posizione del div
-            div.style.left = (event.clientX - distance) + 'px';
-            div.style.top = (event.clientY - distance) + 'px';
-        });
     }
 }
 </script>
 
 <template>
     <div class="container">
-        <h2>Who Am I</h2>
-        <!-- ---------------- -->
-        <div id="movableDiv" @click="trick()">movableDiv</div>
-        <!-- ---------------- -->
-        <div class="bioCont">
-            <div class="leftBio">
-                <div class="line"></div>
-                <!-- <p>
-                    My job is to instruct machines to express emotions.
-                    <br>
-                    ...
-                    <br>
-                    Code and Digital Painting have some major connections: <br>
-                    It's a matter of problem-solving, addressing issues, learning languages, and finding a personal way to convey concepts.
-                    <br>
-                    ...
-                    <br>
-                    I also have this connected duality inside me, a logical calculator and a passionate communicator.
-                </p> -->
-
-                <p>
-                    test
-                    Hey there! I've been into problem-solving and connecting with people ever since I was a kid. I love to disassembly Legos and exploring how and why things worked around me.
-
-                    Growing up, I discovered a more powerful toy: the computer. I used it for both gaming and experimenting with photo editing through Photoshop. This shaped a my connection with the digital world and creativity.
-
-                    As I matured, I decided to take my passion for digital art to the next level. I delved into digital painting, using the powerful digital medium to communicate uniquely. Beside a passion for game design this became a full time job in the world of tabletop role playing games.
-
-                    The journey didn't stop there. It evolved into coding, becoming my direct language with machines. Coding opened up a world of possibilities, allowing me to translate my creative concepts even more precisely and powerfully. Now, my life is an intersection of problem-solving, digital painting, and coding where the passion for communication blends with the magic of the digital realm.
-                </p>
-
-                <!-- <p>
-                    
-                    <br>
-                    <br>
-                    A childhood building LEGO Technic e playing video games in the basement.
-                    <br>
-                    ...
-                    <br>
-                    Using Photoshop to edit tuscanian hills around my parent's house.
-                    <br>
-                    ...
-                    <br>
-                    A teenage using digital art to connect with others.
-                    <br>
-                    ...
-                    <br>
-                    Digital paints and graphics became my passion and job during these years as Role Playing Games did.
-                    <br>
-                    ...
-                    <br>
-                    Now I know CODE I can speak directly to machines using them to create speacial connections with peoples.
-                </p> -->
-
-            </div>
-            <div class="imgWrapper">
-                <img src="../assets/img/FotoCv_SQUARE2.jpg" alt="a">
-
+        <div class="skillsContainer">
+    
+            <div class="ideCont">
+                <div class="topSec">
+                    <h6>Who Am I</h6>
+                </div>
+    
+                <div class="middleSec">
+                    <div class="col-left">
+                        <div class="imgWrapper">
+                    <img src="../assets/img/FotoCv_SQUARE2.jpg" alt="a">
+    
+                </div>
+                    </div> 
+                    <div class="col-right">
+                        Hey there! I've been into problem-solving and connecting with people ever since I was a kid. I love to disassembly Legos and exploring how and why things worked around me.
+    
+                        Growing up, I discovered a more powerful toy: the computer. I used it for both gaming and experimenting with photo editing through Photoshop. This shaped a my connection with the digital world and creativity.
+    
+                        As I matured, I decided to take my passion for digital art to the next level. I delved into digital painting, using the powerful digital medium to communicate uniquely. Beside a passion for game design this became a full time job in the world of tabletop role playing games.
+    
+                        The journey didn't stop there. It evolved into coding, becoming my direct language with machines. Coding opened up a world of possibilities, allowing me to translate my creative concepts even more precisely and powerfully. Now, my life is an intersection of problem-solving, digital painting, and coding where the passion for communication blends with the magic of the digital realm.
+                    </div>
+                </div>
             </div>
         </div>
+   
+
 
         <div class="lowerSection">
             <h3 class="mx-2 text-align-end">use &#9660; to Home</h3>
@@ -117,6 +69,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss'as *;
 
 #movableDiv {
     position: absolute;
@@ -142,10 +95,8 @@ export default {
     display: flex;
     justify-content: space-between;
     padding: 1rem;
-
     .leftBio {
         padding: 1rem 0;
-
         p {
             padding: 1rem 0;
         }
@@ -154,12 +105,10 @@ export default {
     .imgWrapper {
         width: 500%;
         padding: 1rem;
-
         img {
             border: 2px solid rgb(143, 143, 143);
             padding: 0.4rem;
-            border-radius: 3px;
-            
+            border-radius: 3px;   
         }
     }
 }
@@ -168,45 +117,55 @@ h3 {
     text-transform: uppercase;
     font-weight: 400;
 }
-
 .downWrapper {
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 
-.pageDown {
-    padding: 0 1rem;
-    position: relative;
-    cursor: pointer;
-    width: 100px;
-    margin: 0 auto;
-
-    .iconDown {
-
-        .keyBodyDown {
-            width: 50px;
-            height: 45px;
-            border: 3px solid white;
-            // border-radius: 3px;
-            position: relative;
-
-            .arrowDown {
-                position: absolute;
-                top: 50%;
-                left: 49%;
-                transform: translate(-50%, -50%);
-                font-size: 1.5rem;
-            }
-        }
-    }
-
+.ideCont {
+    height: 100%;
+    margin: 2rem;
+    padding-top: 0;
+    border: 2px solid #366686;
+    border-radius: 5px;
 }
 
-.line {
+.topSec {
     width: 100%;
-    height: 1px;
-    border-top: 2px solid white;
+    text-align: start;
+    margin: 0 auto;
+    border-bottom: 2px solid #366686;
 
-    // margin-bottom: 2rem;
-}</style>
+    h6 {
+        width: 30%;
+        padding: 2rem;
+        padding-top: 0.5rem;
+        padding-left: 1rem;
+        padding-bottom: 0.5rem;
+        font-weight: 500;
+        font-size: 1rem;
+        border-right: 2px solid #366686;
+    }
+}
+
+.middleSec {
+    display: flex;
+
+    .col-left {
+        width: 30%;
+        border-right: 2px solid #366686;
+        padding: 2rem;
+        padding-left: 1rem;
+
+    }
+
+    .col-right {
+        height: 100%;
+        width: 70%;
+        padding: 2rem;
+        padding-bottom: 0;
+
+    }
+}
+</style>
